8b59d30dcf7e21aa1ff3d386529eb4700d20be28
mysoyul <vega2k_@naver.com>
2025-02-21 00:05:05 +0900

    tag명 변경

--- src/main/java/com/bookmarker/api/BookMarkerApiApplication.java
+++ src/main/java/com/bookmarker/api/BookMarkerApiApplication.java
@@ -21,7 +21,6 @@ public class BookMarkerApiApplication {
 	@Bean
 	public FilterRegistrationBean<?> corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		//configuration.setAllowedOrigins(List.of("*"));
 		configuration.setAllowedOriginPatterns(List.of("*"));
 		configuration.setAllowCredentials(true);
 		configuration.setAllowedHeaders(List.of(
